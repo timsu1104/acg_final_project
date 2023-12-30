0481045e75ea19cbd3c3a599e39c9bfa40fddedb
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2023-12-30 21:03:13 +0800

    Hardware for MIS.

--- src/sparks/shaders/vertex.glsl
+++ src/sparks/shaders/vertex.glsl
@@ -19,3 +19,56 @@ Vertex GetVertex(uint index) {
   vertex.tex_coord = vec2(vertices[offset + 9], vertices[offset + 10]);
   return vertex;
 }
+
+float GetArea(uint idx) {
+
+  float area_ = 0;
+  uint v_offset = object_infos[idx].vertex_offset;
+  uint i_offset = object_infos[idx].index_offset;
+  uint v_offset_up, i_offset_up;
+  if (idx == object_infos.length() - 1) {
+    v_offset_up = vertices.length();
+    i_offset_up = indices.length();
+  } else {
+    v_offset_up = object_infos[idx + 1].vertex_offset;
+    i_offset_up = object_infos[idx + 1].index_offset;
+  }
+  for (uint i = i_offset; i < i_offset_up; i += 3) {
+    uint j = i + 1, k = i + 2;
+    Vertex v0 = GetVertex(v_offset+indices[i]);
+    Vertex v1 = GetVertex(v_offset+indices[j]);
+    Vertex v2 = GetVertex(v_offset+indices[k]);
+    float area = length(cross(v1.position - v0.position, v2.position - v0.position)) * 0.5;
+    area_ += area;
+  }
+  return area_;
+}
+
+vec3 sample_mesh(uint idx, float total_area) {
+
+  uint v_offset = object_infos[idx].vertex_offset;
+  uint i_offset = object_infos[idx].index_offset;
+  uint i_offset_up;
+  float sampled_float = RandomFloat() * total_area;
+  float u = RandomFloat();
+  float v = RandomFloat();
+
+  if (idx == object_infos.length() - 1) {
+    i_offset_up = indices.length();
+  } else {
+    i_offset_up = object_infos[idx + 1].index_offset;
+  }
+  for (uint i = i_offset; i < i_offset_up; i += 3) {
+    uint j = i + 1, k = i + 2;
+    Vertex v0 = GetVertex(v_offset+indices[i]);
+    Vertex v1 = GetVertex(v_offset+indices[j]);
+    Vertex v2 = GetVertex(v_offset+indices[k]);
+    float area = length(cross(v1.position - v0.position, v2.position - v0.position)) * 0.5;
+    if (sampled_float <= area || i >= i_offset_up - 3) {
+      return v0.position * (1.0 - u) + v1.position * (u * (1.0 - v)) + v2.position * (u * v);
+    }
+    sampled_float -= area;
+  }
+  // Unreachable
+  return vec3(0.0);
+}
