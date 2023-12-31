4f09f7cc1bc012fb4fde3d3d08e3db4bf09ccd03
Zhengyuan Su <su-zy21@mails.tsinghua.edu.cn>
2023-12-31 14:37:38 +0800

    Ameliorate the homogeneous volumetric rendering.

--- src/sparks/shaders/random.glsl
+++ src/sparks/shaders/random.glsl
@@ -56,3 +56,25 @@ vec3 RandomOnSphere() {
 vec3 RandomInSphere() {
   return RandomOnSphere() * pow(RandomFloat(), 0.3333333333333333333);
 }
+
+vec3 HenyeyGreenstein(vec3 direction, float g) {
+  float cosTheta;
+  float u0 = RandomFloat(), u1 = RandomFloat();
+  if (abs(g) < 1e-3)
+      cosTheta = 1.0 - 2.0 * u0;
+  else {
+      float sqrTerm = (1.0 - g * g) / (1.0 - g + 2.0 * g * u0);
+      cosTheta = (1.0 + g * g - sqrTerm * sqrTerm) / (2.0 * g);
+  }
+  float sinTheta = sqrt(max(0.0, 1.0 - cosTheta * cosTheta));
+  float phi = 2.0 * PI * u1;
+  
+  vec3 v1, v2;
+  if (abs(direction.x) > abs(direction.y))
+    v1 = vec3(-direction.z, 0, direction.x) / sqrt(direction.x * direction.x + direction.z * direction.z);
+  else
+    v1 = vec3(0, direction.z, -direction.y) / sqrt(direction.y * direction.y + direction.z * direction.z);
+  v2 = cross(direction, v1);
+
+  return sinTheta * cos(phi) * v1 + sinTheta * sin(phi) * v2 + cosTheta * direction;
+}
